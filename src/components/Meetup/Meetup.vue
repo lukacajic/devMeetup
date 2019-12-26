f1c462c146b20042e094200ce15e83dd567dee3c
Luka Cajic <lukacajic@gmail.com>
2019-12-26 10:46:34 -0600

    Added Vuex to the project

--- src/components/Meetup/Meetup.vue
+++ src/components/Meetup/Meetup.vue
@@ -15,7 +15,6 @@
             <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci assumenda at deserunt dolorum eius esse eum, expedita hic illo ipsum magni, nobis, odio odit quaerat quibusdam quis rem veritatis voluptas!</div>
           </v-card-text>
           <v-card-actions>
-            <v-spacer></v-spacer>
             <v-btn class="primary">Register</v-btn>
           </v-card-actions>
         </v-card>
